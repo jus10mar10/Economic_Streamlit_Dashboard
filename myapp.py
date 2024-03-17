d92f4a7a9d2e0fee4f55f0d2c78ffe5ba3ab69bf
Justin <justinmartin8421@gmail.com>
2024-03-17 16:38:16 -0400

    add page navigation

--- myapp.py
+++ myapp.py
@@ -1,38 +1,46 @@
 import streamlit as st
 import plotly.express as px
 from modules.treasury import yield_curve_full_data_pull
+
+# add page selection
+page = st.sidebar.radio('Select Page', ('Home', 'Yield Curve Viewer'))
+
+if page == 'Home':
+    st.title('Home')
+
+# PAGE: Yield Curve Viewer
+if page == 'Yield Curve Viewer':
     
-st.title('Yield Curve Viewer')
+    @st.cache_data
+    def data_pull_yields():
+        data = yield_curve_full_data_pull()
+        return data
+        
+    st.title('Yield Curve Viewer')
 
-@st.cache_data
-def data_pull():
-    data = yield_curve_full_data_pull()
-    return data
+    data = data_pull_yields()
 
-data = data_pull()
+    dates = st.multiselect('Select Date', data.columns, default=list(data.columns))
+    checkbox = st.checkbox('Show Data')
+    if checkbox:
+        st.write(data[dates])
 
-dates = st.multiselect('Select Date', data.columns, default=list(data.columns))
-checkbox = st.checkbox('Show Data')
-if checkbox:
-    st.write(data[dates])
-
-fig = px.line(data[dates], x=data.index, y=dates, title='Yield Curve', 
-              labels={'value': 'Yield (%)', 'term': 'Term (Years)'})
-# line width
-fig = fig.update_traces(line=dict(width=4))
-# round line caps
-fig = fig.update_traces(line_shape='spline')
-# format y axis as percentage round to 2 decimal places
-fig = fig.update_yaxes(tickformat=".2%")
-# legend at top right
-fig = fig.update_layout(legend=dict(
-    orientation="h",
-    yanchor="bottom",
-    y=1.02,
-    xanchor="right",
-    x=1
-))
-# legend title
-fig = fig.update_layout(legend_title_text='Effective Date')
-st.plotly_chart(fig)
-    
+    fig = px.line(data[dates], x=data.index, y=dates, title='Yield Curve', 
+                labels={'value': 'Yield (%)', 'term': 'Term (Years)'})
+    # line width
+    fig = fig.update_traces(line=dict(width=4))
+    # round line caps
+    fig = fig.update_traces(line_shape='spline')
+    # format y axis as percentage round to 2 decimal places
+    fig = fig.update_yaxes(tickformat=".2%")
+    # legend at top right
+    fig = fig.update_layout(legend=dict(
+        orientation="h",
+        yanchor="bottom",
+        y=1.02,
+        xanchor="right",
+        x=1
+    ))
+    # legend title
+    fig = fig.update_layout(legend_title_text='Effective Date')
+    st.plotly_chart(fig)
