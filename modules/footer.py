2fc89daeb43d60319dcb2ff39dac7cce14e88eda
Justin <justinmartin8421@gmail.com>
2024-03-17 19:42:28 -0400

    add portfolio link

--- modules/footer.py
+++ modules/footer.py
@@ -5,6 +5,7 @@ def footer():
 
     st.text('Created by Justin Martin')
 
-    col1, col2, col3 = st.columns([1, 1, 5])
+    col1, col2, col3, col4 = st.columns([1, 1, 1, 5])
     col1.link_button('LinkedIn', 'https://www.linkedin.com/in/justin-martin-40a680140/')
     col2.link_button('GitHub', 'https://github.com/jus10mar10')
+    col3.link_button('Project Code', 'https://github.com/jus10mar10/Economic_Streamlit_Dashboard')
