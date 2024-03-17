bbb925034a3b7ba69aee0fa10ede528f4c7d9f27
Justin <justinmartin8421@gmail.com>
2024-03-17 19:56:00 -0400

    button wraping fix

--- modules/footer.py
+++ modules/footer.py
@@ -5,7 +5,7 @@ def footer():
 
     st.text('Created by Justin Martin')
 
-    col1, col2, col3, col4 = st.columns([1, 1, 1, 4])
+    col1, col2, col3, col4 = st.columns([1, 1, 2, 3])
     col1.link_button('LinkedIn', 'https://www.linkedin.com/in/justin-martin-40a680140/')
     col2.link_button('GitHub', 'https://github.com/jus10mar10')
     col3.link_button('Project Code', 'https://github.com/jus10mar10/Economic_Streamlit_Dashboard')
